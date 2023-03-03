67276b1a2c7fc682504d347faa008fe7b82ab22c
Jbonez87 <jcastrillon087@gmail.com>
2023-03-03 10:13:47 -0500

    add unicode-segmentation package

--- src/string_practice.rs
+++ src/string_practice.rs
@@ -1,3 +1,5 @@
+use unicode_segmentation::UnicodeSegmentation;
+
 pub struct Template {
     pub text: String,
 }
@@ -99,11 +101,23 @@ fn string_value_types() {
     // Bytes
     // [224, 164, 168, 224, 164, 174, 224, 164, 184, 224, 165, 141, 224, 164, 164, 224, 165, 135]
 
+    for b in "नमस्ते".bytes() {
+        println!("{}", b);
+    }
+
     // Scalar values
     // ['न', 'म', 'स', '्', 'त', 'े']
 
+    for c in "नमस्ते".chars() {
+        println!("{}", c);
+    }
+
     // Graphene Clusters
     // ["न", "म", "स्", "ते"]
+
+    for g in "नमस्ते".graphemes(true) {
+        println!("{}", g);
+    }
 
     println!("{s}");
 }
